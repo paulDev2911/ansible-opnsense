6d15dbe515e688a357728ff045b6754983ed5a26
paulDev2911 <ph24311@tutamail.com>
2026-06-24 21:29:33 +0200

    fix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { config.allowUnfree = true; } }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
     ansible
     python3Packages.httpx
     python3Packages.pip
+    claude-code
   ];
 }
